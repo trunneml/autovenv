592360ec16527c2132fb83e024426c3a9f280a4a
Michael Trunner <michael@trunner.de>
2017-05-30 22:28:16 +0200

    fixed license header

--- autovenv.py
+++ autovenv.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
-#
-# Copyright (C) 2017 Michael Trunner. New BSD License.
-#
+"""
+autovenv -- helper script for pip and venv
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import hashlib
 import logging
 import os
@@ -233,7 +245,7 @@ def main(args=None):
         '--no-index', action='store_false', dest='use_index', default=True,
         help='Force disable pip index.')
     parser.add_argument(
-        'mode', choices=['setup', 'freeze', 'download'],
+        'mode', choices=['setup', 'download'],
         default='setup', nargs='?',
         help="Use 'setup' to create the venv.\n"
              "Use 'freeze' to lock all requirements to the current version.\n"
